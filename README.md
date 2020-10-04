cd418cf0029b8b981c2801569ae9b7c8fc756483
stephenlai <stephenlai2015@gmail.com>
2020-10-04 12:31:59 +0800

    commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# PWA Tutorial
+
+## The Web App Manifest
+
+## Service Workers (js file)
+> javascript files run on main thread
+> service worker runs on separate thread
+
+### Features
+- Load content offline
+- Use background sync
+- Use push notifications
+
+### What does Service Worker do ?
+- listen for fetch requests, push messages, etc.
+  
+## Service Worker Lifecycle
+> Place sw.js in the root directory to control all files/directories
+- register the service worker (sw.js) (O)
+- install event ()
+- service worker become active
+- active event
+- service worker 'listens' for events
+
+## Registering a Service Worker
+
+## install event
+- 把伺服器的資源 (檔案, 圖片等) 安裝到瀏覽器的快取存儲
+- 只要伺服器有變動 (檔案內容修改, 圖片新增刪除), 瀏覽器都匯到伺服器撈取要安裝的資源到快取存儲 
+- Service Worker 必須在伺服器, 不能安裝到快取存儲, 不然快取檔案永遠不能更新 !!!
+
+## active event
+
+## the install banner 
+- connects your mobile phone through use to your computer, the install banner will prompt up !
+
+## Offline Mode
+
+## Pre-Caching Assets (Offline experiences)
+
+## Cache Versioning
+- 伺服器端的檔案更動, 我們同時也要修改 Service Worker 版本
+- 伺服器通知瀏覽器檔案有異動
+- 瀏覽器到伺服器取得更新資源, 除舊版快取, 安裝新版快取
+  
+## Dynamic Cache
+we don't want to precache about and contact pages, we want these two pages dynamically cached,
+so we 
+
+## Offline Fallback Page
+404 page
+
+## Conditional Fallbacks
+
+## Limiting Cache Size
+
+
+Versions:
+- v14: add maskable icon in manifest
+- v15: change title in home page
+- v16: change title in home page again
+- v17: update title in home page 
+- v18: update title in home page 
---
+++ sw.js
@@ -0,0 +1,90 @@
+const staticCache = 'site-static-v18' 
+const dynamicCache = 'site-dynamic-v3'
+
+// App shell assets
+const assets = [ 
+  '/', // storing request url
+  '/index.html',
+  '/pages/fallback.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+]
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size))
+      }
+    })
+  })
+}
+
+// Install service worker
+self.addEventListener('install', evt => {
+  // console.log('service worker has been installed')
+  
+  evt.waitUntil( 
+    caches.open(staticCache)
+    .then(cache => {
+      // reach out to server and all assets
+      console.log('caching shell assets');
+      cache.addAll(assets)
+    })
+  )
+  
+  // Manually skip waiting to activate or check 'Update on reload'
+  self.skipWaiting(); 
+
+})
+
+// activate service worker 
+self.addEventListener('activate', evt => {
+  // console.log('service worker has been activated');  
+
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      // console.log(keys);
+
+      // 刪除舊版 cache
+      return Promise.all(keys
+        .filter(key => key !== staticCache && key !== dynamicCache)
+        .map(key => caches.delete(key))
+      )
+    })
+  )
+})
+
+// fetch event 
+// cache first (offline experiences)
+self.addEventListener('fetch', evt => {
+  // console.log('fetch event', evt)
+
+  evt.respondWith(
+    caches.match(evt.request)
+    .then(cacheRes => {
+      // if cache has assets, fetch assets from cache
+      // if cache is for some reason has missing file, send request to server and install a copy to cache
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCache).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone())
+          limitCacheSize(dynamicCache, 15)
+          return fetchRes
+        })
+      })
+    }).catch(() => { 
+      // 如果圖片資源不在快取存儲, 或者 offline, 重新導向 fallback.html 
+      if (evt.request.url.indexOf('.html') > -1) {
+        return caches.match('/pages/fallback.html')
+      }
+    })
+  )
+})
+
